076ceec823fe4aa33f38bf777d8e667406abe588
notlucaslee <lee.lucas.hyunguk@gmail.com>
2021-11-30 03:26:48 +0000

    added logout feature

--- express_server.js
+++ express_server.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -31,7 +34,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -42,19 +46,24 @@ app.post("/urls", (req, res) => {
   const shortLink = generateRandomString()
   urlDatabase[shortLink] = req.body.longURL;
   console.log(urlDatabase);
-  res.redirect("/urls");         // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls");         
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = { 
     title: 'My URLs', 
-    urls: urlDatabase 
+    urls: urlDatabase, 
+    username: req.cookies["username"],
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  }
   res.render("urls_show", templateVars);
 });
 
@@ -66,19 +75,25 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   console.log(urlDatabase);
-  res.redirect("/urls")
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls")
+  res.redirect("/urls");
 })
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: 'Hello' };
-  res.render("hello_world", templateVars);
-});
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username) 
+  res.redirect("/urls");
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls")
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
